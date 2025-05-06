680551c4d41992da8d1234939d04ef61f15eaa91
Clay Beach <wcbeach30@gmail.com>
2025-05-06 17:02:56 +0000

    now the text of the card rank changes color as needed to reflect the suit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function () {
   //write your code here
   loadRandomCard();
-  document.getElementById("drawCard").onclick = loadRandomCard();
 };
 
-function loadRandomCard(){
+document.getElementById("drawCard").addEventListener('click', loadRandomCard);
+
+function loadRandomCard() {
   // set up the card deck 
   const cardRanks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "J", "Q", "K"]
   const cardSuits = ["❤️", "♠️", "♣️", "♦️"];
@@ -19,9 +16,27 @@ function loadRandomCard(){
   // randomizer,and somewhere in here I will set up a new array with the cards to make sure that once we draw a card, then we can shuffle the deck, still working on this one
   const randomIndex = Math.floor(Math.random() * cardSuits.length);
   const randomCardIndex = Math.floor(Math.random() * cardRanks.length);
-      // pick the suit
+  // pick the suit
   const randomSuit = cardSuits[randomIndex];
   const randomCard = cardRanks[randomCardIndex];
+  console.log(randomSuit);
+  switch (randomSuit) {
+  case "❤️":
+    document.getElementById("displayRandomCardRank").className = "red";
+    break;
+  case "♠️":
+    document.getElementById("displayRandomCardRank").className = "black";
+    break;
+  case "♣️":
+    document.getElementById("displayRandomCardRank").className = "black";
+    break;
+  case "♦️":
+    document.getElementById("displayRandomCardRank").className = "red";
+    break;
+  default:
+    console.log("Not a valid day.");
+}
+
 
   document.getElementById("displayCardSuit").innerHTML = randomSuit;
   document.getElementById("displayCardSuit1").innerHTML = randomSuit;
