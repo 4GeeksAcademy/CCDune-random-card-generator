3ba1cb8c124c19cea2d5da77d6314bf0b04c3e97
Clay Beach <wcbeach30@gmail.com>
2025-05-06 01:12:48 +0000

    I have the bare bones functionality of the random card generator, need to polish it and add some styling

--- src/app.js
+++ src/app.js
@@ -8,4 +8,16 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const cardRanks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "J", "Q", "K"]
+  const cardSuits = ["❤️", "♠️", "♣️", "♦️"];
+
+  const randomIndex = Math.floor(Math.random() * cardSuits.length);
+  const randomCardIndex = Math.floor(Math.random() * cardRanks.length);
+      // pick the suit
+  const randomSuit = cardSuits[randomIndex];
+  const randomCard = cardRanks[randomCardIndex];
+  document.getElementById("displayCardSuit").innerHTML = randomSuit;
+  document.getElementById("displayCardSuit1").innerHTML = randomSuit;
+  document.getElementById("displayRandomCardRank").innerHTML = randomCard;
+
 };
