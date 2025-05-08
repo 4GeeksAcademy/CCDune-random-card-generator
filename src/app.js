d17aeef93062f73a21979602428a045551b084b8
Clay Beach <wcbeach30@gmail.com>
2025-05-08 00:48:24 +0000

    refactored code, still need to add UI components for the shuffle deck.

--- src/app.js
+++ src/app.js
@@ -4,107 +4,105 @@ window.onload = function () {
   let cardDeck = [];
   shuffleCards();
   loadRandomCard();
-  document.getElementById("drawCard").addEventListener('click', loadRandomCard);
-  
-function loadRandomCard(){
-const [suit, rank] = drawCard(cardDeck);
-switch (suit) {
-case "❤️":
-document.getElementById("displayRandomCardRank").className = "red";
-break;
-case "♠️":
-document.getElementById("displayRandomCardRank").className = "black";
-break;
-case "♣️":
-document.getElementById("displayRandomCardRank").className = "black";
-break;
-case "♦️":
-document.getElementById("displayRandomCardRank").className = "red";
-break;
-default:
-document.getElementById("displayRandomCardRank").className = "red";
-}
-document.getElementById("displayCardSuit").innerHTML = suit;
-document.getElementById("displayCardSuit1").innerHTML = suit;
-document.getElementById("displayRandomCardRank").innerHTML = rank;
-document.getElementById("drawCard").innerHTML = "Pick a card";
-document.getElementById("customCardHeaderText").innerHTML = "Random Card Generator"
-}
+  document.getElementById("drawCard").addEventListener("click", loadRandomCard);
 
-
-
-
-function drawCard(deck) {
-  if (deck.length == 0){
-    console.log("You have no cards left!")
-    shuffleCards();
-  } else {
-const i = Math.floor(Math.random() * deck.length);
-console.log(i, deck);
-// splice returns an array of removed elements
-const [card] = deck.splice(i, 1); 
-return card;  // e.g. ["♣️","7"]
+  function loadRandomCard() {
+    const [suit, rank] = drawCard(cardDeck);
+    switch (suit) {
+      case "❤️":
+        document.getElementById("displayRandomCardRank").className = "red";
+        break;
+      case "♠️":
+        document.getElementById("displayRandomCardRank").className = "black";
+        break;
+      case "♣️":
+        document.getElementById("displayRandomCardRank").className = "black";
+        break;
+      case "♦️":
+        document.getElementById("displayRandomCardRank").className = "red";
+        break;
+      default:
+        document.getElementById("displayRandomCardRank").className = "red";
+    }
+    document.getElementById("displayCardSuit").innerHTML = suit;
+    document.getElementById("displayCardSuit1").innerHTML = suit;
+    document.getElementById("displayRandomCardRank").innerHTML = rank;
+    document.getElementById("drawCard").innerHTML = "Pick a card";
+    document.getElementById("customCardHeaderText").innerHTML =
+      "Random Card Generator";
   }
-  
-}     
 
-function shuffleCards(){
-  cardDeck =   [["❤️","A"],
-  ["❤️","2"], 
-  ["❤️","3"],
-  ["❤️","4"],
-  ["❤️","5"],
-  ["❤️","6"],
-  ["❤️","7"],
-  ["❤️","8"],
-  ["❤️","9"],
-  ["❤️","10"],
-  ["❤️","11"],
-  ["❤️","J"],
-  ["❤️","Q"],
-  ["❤️","K"],
-  ["♠️","A"],
-  ["♠️","2"], 
-  ["♠️","3"],
-  ["♠️","4"],
-  ["♠️","5"],
-  ["♠️","6"],
-  ["♠️","7"],
-  ["♠️","8"],
-  ["♠️","9"],
-  ["♠️","10"],
-  ["♠️","11"],
-  ["♠️","J"],
-  ["♠️","Q"],
-  ["♠️","K"],
-  ["♣️","A"],
-  ["♣️","2"], 
-  ["♣️","3"],
-  ["♣️","4"],
-  ["♣️","5"],
-  ["♣️","6"],
-  ["♣️","7"],
-  ["♣️","8"],
-  ["♣️","9"],
-  ["♣️","10"],
-  ["♣️","11"],
-  ["♣️","J"],
-  ["♣️","Q"],
-  ["♣️","K"],
-  ["♦️","A"],
-  ["♦️","2"], 
-  ["♦️","3"],
-  ["♦️","4"],
-  ["♦️","5"],
-  ["♦️","6"],
-  ["♦️","7"],
-  ["♦️","8"],
-  ["♦️","9"],
-  ["♦️","10"],
-  ["♦️","11"],
-  ["♦️","J"],
-  ["♦️","Q"],
-  ["♦️","K"]];
-}
+  function drawCard(deck) {
+    if (deck.length == 0) {
+      console.log("You have no cards left!");
+      shuffleCards();
+    } else {
+      const i = Math.floor(Math.random() * deck.length);
+      console.log(i, deck);
+      // splice returns an array of removed elements
+      const [card] = deck.splice(i, 1);
+      return card; // e.g. ["♣️","7"]
+    }
+  }
 
+  function shuffleCards() {
+    cardDeck = [
+      ["❤️", "A"],
+      ["❤️", "2"],
+      ["❤️", "3"],
+      ["❤️", "4"],
+      ["❤️", "5"],
+      ["❤️", "6"],
+      ["❤️", "7"],
+      ["❤️", "8"],
+      ["❤️", "9"],
+      ["❤️", "10"],
+      ["❤️", "11"],
+      ["❤️", "J"],
+      ["❤️", "Q"],
+      ["❤️", "K"],
+      ["♠️", "A"],
+      ["♠️", "2"],
+      ["♠️", "3"],
+      ["♠️", "4"],
+      ["♠️", "5"],
+      ["♠️", "6"],
+      ["♠️", "7"],
+      ["♠️", "8"],
+      ["♠️", "9"],
+      ["♠️", "10"],
+      ["♠️", "11"],
+      ["♠️", "J"],
+      ["♠️", "Q"],
+      ["♠️", "K"],
+      ["♣️", "A"],
+      ["♣️", "2"],
+      ["♣️", "3"],
+      ["♣️", "4"],
+      ["♣️", "5"],
+      ["♣️", "6"],
+      ["♣️", "7"],
+      ["♣️", "8"],
+      ["♣️", "9"],
+      ["♣️", "10"],
+      ["♣️", "11"],
+      ["♣️", "J"],
+      ["♣️", "Q"],
+      ["♣️", "K"],
+      ["♦️", "A"],
+      ["♦️", "2"],
+      ["♦️", "3"],
+      ["♦️", "4"],
+      ["♦️", "5"],
+      ["♦️", "6"],
+      ["♦️", "7"],
+      ["♦️", "8"],
+      ["♦️", "9"],
+      ["♦️", "10"],
+      ["♦️", "11"],
+      ["♦️", "J"],
+      ["♦️", "Q"],
+      ["♦️", "K"],
+    ];
+  }
 };
